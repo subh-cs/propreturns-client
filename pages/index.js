c2aefd6ca75b5025a4205da3ce78c6997a2f7d64
Subhodip Roy <75121304+subh-cs@users.noreply.github.com>
2023-02-01 10:32:16 +0530

    URL of apollo graphql server has changed

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3001/graphql",
+  uri: "https://propreturnsgrapqlserver.vercel.app/graphql",
   cache: new InMemoryCache(),
 });
 
